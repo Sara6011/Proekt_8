53cc9130f7c046aa5717c27097f29147f09077b9
Jankulovska <jankulovskasara60@gmail.com>
2023-11-21 18:54:28 +0100

    Add first java program

---
+++ src/zadaca7/Pravoagolnik.java
@@ -0,0 +1,24 @@
+package zadaca7;
+import java.util.Scanner;
+public class Pravoagolnik {
+
+	public static void main(String[] args) {
+		double str1, str2, povrsina, perimetar;
+	    Scanner tastatura=new Scanner (System.in);
+	    System.out.print("Programata presmetuva povrsina i perimetar na prvoagolnik");
+	    System.out.print("Vnesete dolzina na edna strana:");
+	    str1=tastatura.nextDouble();
+	    System.out.print("Vnesete dolzina na duga strana");
+	    str2=tastatura.nextDouble();
+	    povrsina=str1*str2;
+	    perimetar=2*str1+2*str2;
+	    System.out.print("Perimetarot e: " + perimetar);
+	    //System.out.print(perimetar);
+	    System.out.print("Plostinata e: " + povrsina);
+	    //System.out.print(povrsina);
+	    
+	    
+
+	}
+
+}
